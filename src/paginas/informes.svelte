407935d50cd12a02ed2b91db796717b138fa87d8
Yilin <yilin.zh135@gmail.com>
2024-12-05 19:14:33 +0100

    Agregar botón de descargar informe

--- src/paginas/informes.svelte
+++ src/paginas/informes.svelte
@@ -14,18 +14,21 @@
         overflow: hidden;
         position: relative;
     }
-    .seccion{
-            display: flex;
-            flex-direction: row;
-            flex-wrap: wrap;
-            justify-content: space-around;
-            text-align: center;
-            align-items: center;
-            min-height: 85vh;
-            width: 100%;
-            padding-bottom: 100px;
-            margin-top: 50px;
-        }
+    p{
+        margin-bottom: 0;
+    }
+    .seccion {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        text-align: center;
+        align-items: center;
+        min-height: 85vh;
+        width: 100%;
+        padding-bottom: 100px;
+        margin-top: 50px;
+    }
     .cuadrados {
         display: flex;
         flex-direction: row;
@@ -38,21 +41,51 @@
         width: 100%;
         margin-bottom: 100px;
     }
-    .graficas h3{
+    .graficas h3 {
         margin-top: 50px;
     }
 
-    .datos{
+    .datos {
         width: 100%;
     }
+    .downloadBtnCont {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .downloadBtn {
+        border-radius: 10px;
+        background-color: #fde379;
+        font-size: 20px;
+        border: none;
+        font-weight: bold;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        padding: 10px;
+    }
+    .downloadBtn:hover {
+        transition: all 0.3s ease;
+        cursor: pointer;
+        background-color: #edaf52;
+
+    }
     @media (min-width: 992px) {
+        .downloadBtnCont{
+            justify-content: end;
+        }
+        .downloadBtn{
+            width: 200px;
+        }
         main {
             background-color: rgb(255, 255, 255);
             margin-left: 16%;
             height: 95vh;
             overflow-y: scroll;
             border-radius: 35px;
-
         }
 
         .graficas {
@@ -62,7 +95,7 @@
             margin-bottom: 0px;
             width: 50%;
         }
-        
+
         main::-webkit-scrollbar {
             width: 0px;
             height: 0px;
@@ -73,11 +106,10 @@
         main::-webkit-scrollbar-thumb {
             background: transparent;
         }
-        .seccion{
-            padding:0 ;
+        .seccion {
+            padding: 0;
             gap: 0px;
             background-color: white;
-
         }
         .graficas,
         .datos {
@@ -86,17 +118,23 @@
         }
     }
 </style>
+
 <main class={cols}>
     <h1>Informe de Ventas</h1>
     <div class="seccion">
-    <div class="graficas">
-        <h3>Productos mejor vendidos</h3>
-        <Graphic1 />
-        <h3>Ganancias del año</h3>
-        <Graphic2 />
-    </div>
-    <div class="datos">
-        <PedidosGastos />
-    </div>
+        <div class="downloadBtnCont">
+            <div class="downloadBtn">
+                <p>Descargar informe</p>
+            </div>
+        </div>
+        <div class="graficas">
+            <h3>Productos mejor vendidos</h3>
+            <Graphic1 />
+            <h3>Ganancias del año</h3>
+            <Graphic2 />
+        </div>
+        <div class="datos">
+            <PedidosGastos />
+        </div>
     </div>
 </main>
